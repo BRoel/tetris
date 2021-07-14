016cbfc943df79ecacf949c0248a6ea469c3378f
Brennan Roel <brennanroel@gmail.com>
2021-07-14 14:20:14 -0500

    added css styling to tetris

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -3,20 +3,25 @@ import Stage from './Stage';
 import Display from './Display';
 import StartButton from './StartButton';
 import { createStage } from '../gameHelpers';
+import { StyledTetris, StyledTetrisWrapper } from './styles/StyledTetris';
 
 const Tetris = () => {
     return (
-        <div>
-            <Stage stage={createStage()}/>
-            <aside>
+        <StyledTetrisWrapper>
+            <StyledTetris>
                 <div>
-                    <Display text="Score" />
-                    <Display text="Rows" />
-                    <Display text="Level" />
+                    <Stage stage={createStage()}/>
+                    <aside>
+                        <div>
+                            <Display text="Score" />
+                            <Display text="Rows" />
+                            <Display text="Level" />
+                        </div>
+                        <StartButton />
+                    </aside>
                 </div>
-                <StartButton />
-            </aside>
-        </div>
+            </StyledTetris>
+        </StyledTetrisWrapper>
     )
 }
 
